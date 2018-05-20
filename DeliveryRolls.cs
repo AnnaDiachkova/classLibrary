2ed50028c720700940f4c6fe565ad340b9e90386
AnnaDiachkova <36928308+AnnaDiachkova@users.noreply.github.com>
2018-05-20 20:26:18 +0500

    first commit

---
+++ DeliveryRolls.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeliveryRolls
+{
+    public class RollsRequestDto
+    {
+        /// <summary>
+        /// Дата заполнения
+        /// </summary>
+        public DateTime Filled { get; set; }
+        /// <summary>
+        /// ФИО заказчика
+        /// </summary>
+        public string FullName { get; set; }
+        /// <summary>
+        /// Стоимость
+        /// </summary>
+        public decimal Price { get; set; }
+        public Currency Currency { get; set; }
+    }
+    public enum Currency
+    {
+        Rubles
+    }
+    public class WayPoint
+    {
+        /// <summary>
+        /// Адрес
+        /// </summary>
+        public string Address { get; set; }
+    }
+    public class RollSelectionDto
+    {
+        /// <summary>
+        /// Виды роллов
+        /// </summary>
+       public List<TypesOfRolls> TypesOfRolls { get; set; }
+        /// <summary>
+        /// Дополнительно
+        /// </summary>
+       public List<Additionally> Additionally { get; set; }
+    }
+
+    public enum Additionally
+    {
+        sticks,
+        soySauce,
+        ginger,
+        wasabi,
+    }
+
+    public enum TypesOfRolls
+    {
+        cucumber,
+        salmon,
+        sesame,
+        creamСheese,
+    }
+
+    public class Program
+        {
+            static void Main()
+            { }
+        }
+}
+
+
